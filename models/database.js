0c26cc977d3acdee49a7878cc71fd3cc59f69bec
ashwinbalajidhandapani <98062314+ashwinbalajidhandapani@users.noreply.github.com>
2022-03-03 15:39:34 -0500

    Update database.js

--- models/database.js
+++ models/database.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user:'root',
-    password:'',
-    database:'root',
-    host:'localhost',
+    user:'postgres',
+    password:'passsword',
+    database:'postgres
+    host:'127.0.0.1
     port:5432
 });
 
