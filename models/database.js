fbf400aa2a7ff50ce7046d542522aa9c41d877b4
Ashwin Balaji Dhandapani <dhandapani.as@northeastern.edu>
2022-03-03 15:43:55 -0500

    db changes

--- models/database.js
+++ models/database.js
@@ -3,8 +3,8 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user:'postgres',
     password:'passsword',
-    database:'postgres
-    host:'127.0.0.1
+    database:'postgres',
+    host:'127.0.0.1',
     port:5432
 });
 
