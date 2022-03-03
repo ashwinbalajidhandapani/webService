a23aa33dfffdbc2a84ff3585423e68119412c4d0
ashwinbalajidhandapani <98062314+ashwinbalajidhandapani@users.noreply.github.com>
2022-03-03 15:38:32 -0500

    Update app.sh

--- packer/app.sh
+++ packer/app.sh
@@ -24,32 +24,29 @@ EOF
 sleep 15
 sudo yum makecache -y
 sleep 5
-sudo yum install postgresql14 -y
-sudo yum install postgresql14-server -y
-    # Initializing DB
-sudo /usr/pgsql-14/bin/postgresql-14-setup initdb
-    # Staring PSQL service
-sudo systemctl enable --now postgresql-14
-    # making PSQL listen to port 5432
-echo "#############################"
-echo "## setting the port to 5432##"
-echo "#############################"
-sudo ss -tunelp | grep 5432
-sudo systemctl restart postgresql-14
-sleep 30
-echo "#############################"
-echo "########starting psql########"
-echo "#############################"
-sudo systemctl enable --now postgresql-14
- 
-createDatabase(){
-    echo "creating database"
-    sudo su - postgres
-    psql -c "create database demo"
-}
-createDatabase
-echo "End of Script"
-
+sudo yum install -y gcc-c++ make
+sleep 5
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+sleep 5
+sudo yum install -y nodejs
+# Postgres installation
+sleep 10
+sudo tee /etc/yum.repos.d/pgdg.repo<<EOF
+pgdg14]
+name=PostgreSQL 14 for RHEL/CentOS7 - x86_64
+gpgkey=https://download.postgresql.org/pub/repos/yum/RPM-GPG-KEY-PGDG-14
+baseurl=https://download.postgresql.org/pub/repos/yum/14/redhat/rhel-7-x86_64/
+enabled=1
+gpgcheck=1
+EOF
+sudo yum install postgresql
+cd /usr/pgsql-14/bin
+sudo ./postgresql-14-setup initdb
+sudo systemctl start postgresql-14
+sleep 15
+cd ~
+sudo -u postgres psql -c "alter role postgres with password 'password';"
+sudo -u postgres psql -c "grant all privileges on database postgres to postgres;"
 echo "########################"
 echo "#### Installing Git ####"
 echo "########################"
@@ -59,13 +56,8 @@ sleep 30
 
 mkdir webService
 cd webService
-git clone git@github.com:ashwinBalajiDhadapani-org/webService.git
-files=$(shopt -s nullglob dotglob; echo your/dir/*)
-if (( ${#files} ))
-then
-  echo "contains files"
-else 
-  echo "empty (or does not exist or is a file)"
-fi
-
-
+echo "#######Cloning using HTTPS########"
+git clone https://dhandapani.as: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@github.com/ashwinBalajiDhadapani-org/webService.git
+cd webService
+npm install
+node index.js
