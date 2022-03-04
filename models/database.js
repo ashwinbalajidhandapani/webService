fb6deb4bec020248f47ccc80715961d6d45b52f9
Ashwin Balaji Dhandapani <dhandapani.as@northeastern.edu>
2022-03-04 12:54:46 -0500

    shell file changes in ami file

--- models/database.js
+++ models/database.js
@@ -3,7 +3,7 @@ const mySql = require('mysql');
 const pool = mySql.createPool({
     connectionLimit: 100,
     user:'root',
-    password:'webservice',
+    password:'c$yE6225Cloud',
     database:'webapp',
     host:'127.0.0.1',
     port:3306
