18662456588eaebfbee7e164f62c35da07eb48b9
Ashwin Balaji Dhandapani <dhandapani.as@northeastern.edu>
2022-04-15 02:35:23 -0400

    testing 3

--- index.js
+++ index.js
@@ -438,7 +438,7 @@ app.delete("/v1/user/self/pic",
     });
 
 // Endpoint created for assignment 1
-app.get('/healthz', (req, res) => {
+app.get('/health', (req, res) => {
     console.log('Health Endpoint')
     res.set({
         "readOnly": "true"
