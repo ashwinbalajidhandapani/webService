9d8d68bcafd95743e5d29d23bc0a195320882f59
Ashwin Balaji Dhandapani <dhandapani.as@northeastern.edu>
2022-04-16 08:44:06 -0400

    test final

--- code-deploy/applicationStop.sh
+++ code-deploy/applicationStop.sh
@@ -3,6 +3,6 @@ echo '##### Application Stop Script ####'
 sudo su root
 sudo pm2 stop all
 sudo pm2 save
-cd /home/ec2-user/node-app
+cd /home/ec2-user/node_app
 sudo systemctl stop node-service.service
 sudo systemctl stop amazon-cloudwatch-agent.service
